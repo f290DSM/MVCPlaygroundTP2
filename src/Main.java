10512d1b7840edbf587787d13dc65d573bca0972
BZR4 <BZR4@users.noreply.github.com>
2025-09-10 22:08:34 -0300

    Conexao

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import model.ConexaoFactory;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection conexao = ConexaoFactory.getConexao();
+        ResultSet rst = conexao.createStatement()
+                .executeQuery("select version() as v;");
+
+        if (rst.next()) {
+            System.out.println(rst.getString("v"));
         }
+
     }
 }
---
+++ src/model/ConexaoFactory.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory {
+    private static final String url = "jdbc:mysql://localhost:3306/fatec";
+    private static final String user = "root";
+    private static final String password = "segredo";
+
+    public static Connection getConexao() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
