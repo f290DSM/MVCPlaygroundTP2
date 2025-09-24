29075db5a4db9d0e5675dad1cd3334465469357a
BZR4 <BZR4@users.noreply.github.com>
2025-09-24 20:40:01 -0300

    Inicio CRUD - 01

--- src/model/dao/ContatoMySqlDAO.java
+++ src/model/dao/ContatoMySqlDAO.java
@@ -3,8 +3,11 @@ package model.dao;
 import model.Contato;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,7 +22,7 @@ public class ContatoMySqlDAO {
     }
 
     public void salvar(Contato contato) throws SQLException {
-        String template = "INSERT INTO fatec.contatos(" +
+        String template = "INSERT INTO contatos(" +
                 "nome, email, telefone)" +
                 "VALUES('%s', '%s', '%s');";
 
@@ -35,5 +38,27 @@ public class ContatoMySqlDAO {
             logger.log(Level.SEVERE, "Falha ao salvar contato.", e);
             throw e;
         }
+    }
+
+    public List<Contato> getContatos() throws SQLException {
+        List<Contato> contatos = new ArrayList<>();
+        String sql = "SELECT id, nome, email, telefone FROM contatos;";
+
+        try (Statement stm = connection.createStatement();
+             ResultSet rst = stm.executeQuery(sql)) {
+            while(rst.next()) {
+                Contato contato = new Contato(
+                        rst.getInt("id"),
+                        rst.getString("nome"),
+                        rst.getString("email"),
+                        rst.getString("telefone")
+                );
+                contatos.add(contato);
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Falha ao recuperar contatos.", e);
+        }
+
+        return contatos;
     }
 }
